0451e327c1c09e2fdd1b6b45e4358032ceedd2c1
Yusuf Manshur <ymanshur@gmail.com>
2025-07-14 14:27:44 +0700

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Stock Buy and Sell – Multiple Transaction Allowed/stock-buy-and-sell-multiple-transaction-allowed.cpp
+++ Difficulty: Medium/Stock Buy and Sell – Multiple Transaction Allowed/stock-buy-and-sell-multiple-transaction-allowed.cpp
@@ -2,16 +2,29 @@
 class Solution {
   public:
     int maximumProfit(vector<int> &prices) {
-        // code here
         int total = 0;
         
-        for (int i = 0; i < prices.size() - 1; i++) {
-            int profit = prices[i + 1] - prices[i];
+        // for (int i = 0; i < prices.size() - 1; i++) {
+        //     int profit = prices[i + 1] - prices[i];
             
-            if (profit > 0) {
-                //cout << prices[i + 1] << " " << prices[i] << endl;
-                total += profit;
-            }
+        //     if (profit > 0) {
+        //         //cout << prices[i + 1] << " " << prices[i] << endl;
+        //         total += profit;
+        //     }
+        // }
+        
+        int buy; // min price (local buy)
+        int sell; // max price (local sell) 
+        int n = prices.size();
+        
+        for (int i = 0; i < n - 1;) {
+            while (i < n - 1 && prices[i] > prices[i + 1]) { i++; }
+            buy = prices[i];
+            while (i < n - 1 && prices[i] <= prices[i + 1]) { i++; }
+            sell = prices[i];
+            
+            //cout << buy << " " << sell << endl;
+            total += sell - buy;
         }
         
         return total;
