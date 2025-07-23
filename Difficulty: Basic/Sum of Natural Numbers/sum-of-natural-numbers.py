c5368b2e6b27ad3e3907403c9128ca7cf09832ce
Yusuf Manshur <ymanshur@gmail.com>
2025-07-23 14:47:13 +0700

    Update sum-of-natural-numbers.py

--- Difficulty: Basic/Sum of Natural Numbers/sum-of-natural-numbers.py
+++ Difficulty: Basic/Sum of Natural Numbers/sum-of-natural-numbers.py
@@ -1,8 +1,11 @@
 class Solution:
+    # 1, 2, 3, 4, 5 = ((1 + 5) + (2 + 4) + (3 + 3) + (4 + 2) + (5 + 1)) / 2
+    #               = n * (n + 1) / 2
     def findSum(self, n):
-        sum = 0
-        for i in range(1, n + 1):
-            sum += i
+        # sum = 0
+        # for i in range(1, n + 1):
+        #     sum += i
+        
+        sum = n * (n + 1) // 2
         
         return sum
-        
