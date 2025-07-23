b715a6c93bf3b29861caea9b5ba4dbe1b389b3f3
Yusuf Manshur <ymanshur@gmail.com>
2025-07-23 17:53:15 +0700

    Update series-ap.py

--- Difficulty: Basic/Series AP/series-ap.py
+++ Difficulty: Basic/Series AP/series-ap.py
@@ -1,8 +1,15 @@
 
 class Solution:
+    """
+    d = a2 - a1
+    t1 = a1 + (i - 1) * d
+    t2 = a1 + (i - 2) * d
+    t3 = a1 + (i - 3) * d
+    tn = a1 + (n - 1) * d
+    """
     def nthTermOfAP(self, a1 : int, a2 : int, n : int) -> int:
         d = a2 - a1
-        t = a1
-        for i in range(2, n + 1):
-            t += d
-        return t
+        # tn = a1
+        # for i in range(1, n):
+        #     tn += d
+        return a1 + (n - 1) * d
