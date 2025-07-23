1d03dfacaaa4157dbf062e14633262449f9c1215
Yusuf Manshur <ymanshur@gmail.com>
2025-07-23 21:26:46 +0700

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Largest Element in Array/largest-element-in-array.py
@@ -0,0 +1,10 @@
+class Solution:
+    def largest(self, arr):
+        max = 0
+        for x in arr:
+            if x > max:
+                max = x
+        
+        
+        return max
+        
